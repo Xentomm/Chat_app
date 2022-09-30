1e8d1e97cb288cf07401247d9c2f2047a668452d
Xentomm <erimuslukasz@gmail.com>
2022-09-30 13:57:20 +0200

    v2

--- client.py
+++ client.py
@@ -4,16 +4,49 @@ import threading
 HOST = '127.0.0.1'
 PORT = 1234
 
+def listen_for_messgaes_from_server(client):
+    while 1:
+        message = client.recv(2048).decode('utf-8')
+        if message != "":
+            username = message.split(": ")[0]
+            content = message.split(": ")[1]
+
+            print(f"[{username}] {content}")
+        else:
+            print("Wiadomość od klienta jest pusta")
+
+def send_message_to_server(client):
+    while 1:
+        message = input("Message: ")
+        if message != "":
+            client.sendall(message.encode())
+        else:
+            print("Nie można wysłac pustej wiadomości")
+            exit(0)
+
+def communicate_to_server(client):
+    username = input("Nazwa użytkownika: ")
+    if username != '':
+        client.sendall(username.encode())
+    else:
+        print("Nazwa użytkownika nie może być pusta")
+        exit(0)
+
+    threading.Thread(target = listen_for_messgaes_from_server, args = (client, )).start()
+
+    send_message_to_server(client)
+
 def main():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    # Połączenie z serwerem
+    # Connected to server
     try:
         client.connect((HOST, PORT))
         print(f"Połączono z serwerem")
     except:
         print(f"Nieudanie połaczenie z serwerem {HOST} {PORT}")
 
+    communicate_to_server(client)
 
 if __name__ == '__main__':
     main()
--- server.py
+++ server.py
@@ -1,9 +1,45 @@
+from multiprocessing.connection import Listener
 import socket
 import threading
+from urllib import response
 
 HOST = "127.0.0.1"
 PORT = 1234
 LISTNER_LIMIT = 5
+
+# List of connected users
+active_clients = []
+
+def listen_for_messages(client, username):
+    while 1:
+        message = client.recv(2048).decode('utf-8')
+        if message != '':
+            final_msg = username + ': ' + message
+            send_messages_to_all(final_msg)
+        else:
+            print(f"Nie można wysłać pustej wiadomości od {username}")
+
+# Function to send msg to single connected client
+def send_message_to_client(client, message):
+    client.sendall(message.encode())
+
+# Function to send msg to all connected clients
+def send_messages_to_all(message):
+    for user in active_clients:
+        send_message_to_client(user[1], message)
+
+# Function to handle client
+def client_handler(client):
+    # Server will listen for client msg
+    while 1:
+        username = client.recv(2048).decode('utf-8')
+        if username != '':
+            active_clients.append((username, client))
+            break
+        else:
+            print("Nazwa urzytkownika jest pusta")
+    
+    threading.Thread(target = listen_for_messages, args = (client, username, )).start()
 
 def main():
     # AD_INET, IPv4
@@ -16,12 +52,14 @@ def main():
     except:
         print(f"Nieudane połączenie {HOST} z portem {PORT}")
 
-    # Limit serwera
+    # Server limit
     server.listen(LISTNER_LIMIT)
 
     while 1:
         client, adress = server.accept()
         print(f"Pomyślnie połączono z klientem {adress[0]} {adress[1]}")
+
+        threading.Thread(target = client_handler, args = (client, )).start()
 
 if __name__ == '__main__':
     main()
